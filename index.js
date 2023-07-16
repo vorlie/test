8b0ea6b84db4ebf0e9c80b229778763ea485b6ce
vorlie <vorlie0961@gmail.com>
2023-07-16 15:31:06 +0200

    testset

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const path = require('path');
 const express = require('express');
 
 const app = express();
-
-app.use('',express.static(path.join(__dirname, '/')));
-
 
 app.get('/', (request, response) => {
 	return response.sendFile('index.html', { root: '.' });
