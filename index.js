e5dd7103022045194c16ff7dcde7c93d6f7d3d5e
vorlie <vorlie0961@gmail.com>
2023-07-16 15:30:04 +0200

    TEST

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.use('',express.static(path.join(__dirname, 'public')));
+app.use('',express.static(path.join(__dirname, '/')));
 
 
 app.get('/', (request, response) => {
